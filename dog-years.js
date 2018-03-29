ba729400c552805d4cb5656a87f319672771fdba
Sam Yang <super8989@gmail.com>
2018-03-29 12:30:51 -0400

    dog-years.js

---
+++ dog-years.js
@@ -0,0 +1,16 @@
+const myAge = 29;
+
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+laterYears *= 4;
+
+const myAgeinDogYears = earlyYears + laterYears;
+
+var myName = 'SAM'.toLowerCase();
+
+
+console.log(`My name is ${myName}. I am ${myAgeinDogYears} years old in dog years.`);
+
+console.log('Alternatively... My name is ' + myName + '.' + ' I am ' + myAgeinDogYears + ' years old in dog years.');
